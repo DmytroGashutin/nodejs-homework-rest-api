e961cae768ec3871ea6414582d1863886aef7067
Dmytro Gashutin <dm.gashutin@gmail.com>
2022-08-29 09:09:37 +0300

    hw-03

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require('./app')
 
-app.listen(3022, () => {
-  console.log("Server running. Use our API on port: 3022")
-})
+// app.listen(3022, () => {
+//   console.log("Server running. Use our API on port: 3022")
+// })
